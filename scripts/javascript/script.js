3d3e3aedfac1569864f12fdd361e5bc73fa39ecf
koffiehuis <markmalestein@gmail.com>
2019-01-30 21:03:37 +0100

    .

--- scripts/javascript/script.js
+++ scripts/javascript/script.js
@@ -220,12 +220,15 @@ function initializePieContainer() {
   loadDataPie()
 }
 
-// Loads
+// Loads data for pie-chart
 function loadDataPie() {
+
+  // Initialize pie size
   var widthPie = svgWidth,
       heightPie = 350,
       radius = Math.min(widthPie, heightPie) / 3 + 10;
 
+  // Get sum and values from objects
   var valueArray = [];
   var valueSum = 0;
   for (value of Object.values(filterData("pie"))) {
@@ -236,27 +239,29 @@ function loadDataPie() {
 
   var maxValue = Math.max.apply(Math, valueArray)
 
+  // Makes tooltip
   var toolTip = d3.tip()
     .attr("class", "toolTip")
     .offset([-10, 0])
     .html(function(d) {
-      return "<span class='toolInfo'>" + d.data.categoryTag + "<br/>" + d.data.Quantity  + " GWh" + "</span>";
+      return ("<span class='toolInfo'>" + d.data.categoryTag + "<br/>"
+              + d.data.Quantity  + " GWh" + "</span>");
     })
 
-
+  // Returns arcs for pie-slices
   var arc = d3.arc()
     .outerRadius(radius - 10)
     .innerRadius(0);
 
+  // Returns arcs for slice-labels
   var labelArc = d3.arc()
     .outerRadius(radius - 5)
     .innerRadius(radius - 5);
 
-  var hoverArc = d3.arc().outerRadius(radius + 20)
-
+  // Returns start and end angles for arcs
   var pie = d3.pie()
     .sort(null)
-    . value(function(d) { return parseInt(d.Quantity); });
+    .value(function(d) { return parseInt(d.Quantity); });
 
   pieData = pie(filterData("pie"));
 
@@ -266,17 +271,23 @@ function loadDataPie() {
       return (180 / Math.PI * (d.startAngle + d.endAngle) / 2 - 90);
   }
 
+  // Call the tooltip
   d3.selectAll('.svgPie').call(toolTip)
+
+  // Delete old arcs, if any
   d3.selectAll(".arc").remove()
 
+  // Adds arcs to svg
   var g = d3.select(".svgPie").selectAll(".arc")
      .data(pieData)
    .enter().append("g")
      .attr("class", "arc")
      .attr("transform", "translate(" + svgWidth / 2 + " " + svgHeight / 2 + ")");
 
+  // Removes old arc-paths, if any
   d3.selectAll(".piePath").remove()
 
+  // Adds paths to arcs and gives color based of item-value
   g.append("path")
     .attr("class", "piePath")
      .attr("d", arc)
@@ -292,15 +303,19 @@ function loadDataPie() {
     			}
     		});
 
-      d3.selectAll(".piePath").style("stroke-width", ".5px")
-       .style("stroke", "black")
-     .on("click", function(d) {
-       categoryOption = d.data.categoryTag;
-       document.getElementById("mySelect").value = categoryOption
-       loadDataMap();
-       loadDataLine2();
-       addTitles();
-     })
+  d3.selectAll(".piePath").style("stroke-width", ".5px")
+    .style("stroke", "black")
+
+    // When slice is clicked, reload map, line-chart2 and titles
+    .on("click", function(d) {
+      categoryOption = d.data.categoryTag;
+      document.getElementById("mySelect").value = categoryOption
+      loadDataMap();
+      loadDataLine2();
+      addTitles();
+      })
+
+     // Show tooltip and give accent on hover-over
      .on("mouseover", function(d) {
        toolTip.show(d)
          .style("left", `${d3.event.pageX}px`)
@@ -310,6 +325,8 @@ function loadDataPie() {
          .style("stroke", "black")
          .style("stroke-width", "2px")
      })
+
+     // Hide tooltip and accent when hover-over stops
      .on("mouseout", function() {
        toolTip.hide()
 
@@ -318,13 +335,16 @@ function loadDataPie() {
          .style("stroke-width", ".5px")
      })
 
-
+  // Add labels to slices
   g.append("text")
      .style("text-anchor", "end")
+
+     // Puts labels in correct orientation
      .attr("transform", function(d) {
        centroid = labelArc.centroid(d)
        if (centroid[0] < 0.0) {
-         return "translate(" + centroid + ")" + "rotate(" + (getAngle(d) + 180) + ")";
+         return ("translate(" + centroid + ")" + "rotate(" + (getAngle(d)
+                 + 180) + ")");
 
        }
        else {
@@ -334,6 +354,8 @@ function loadDataPie() {
      })
      .attr("dy", ".35em")
      .attr("class", "pieText")
+
+     // Don't show labels of slices smaller than .5% of total
      .text(function(d) {
        if (parseInt(d.data.Quantity) > 0.005 * valueSum){
          return d.data.categoryTag;
@@ -344,30 +366,30 @@ function loadDataPie() {
      })
      .style("fill", "#565656")
      .style("font-weight", "bold");
-
-
 }
 
 
 function initializeLine1Container() {
+
+  // Remoce old svg
   d3.select(".line1").remove();
 
-
+  // Get correct values needed for linechart
   var dataLine1 = filterData("line1")
 
   var yValues = getValues(dataLine1, categoryOption);
 
-
   var maxY = Math.max.apply(Math, yValues);
 
+  // Sizing of linechart-svg
   var margin = {top: 50, right: 50, left: 50, bottom: 50},
     widthLine1 = svgWidth - margin.left - margin.right,
     heightLine1 = 350 - margin.top - margin.bottom;
 
-
+  // Scaling for X-axis
   var xScale = d3.scaleTime().domain([1990, 2014]).range([0, widthLine1 * .85]);
 
-
+  // Add svg to div
   var svgLine1 = d3.select("#line1Div")
     .append("svg")
     .attr("width", `${svgWidth}`)
@@ -376,14 +398,13 @@ function initializeLine1Container() {
     .append("g")
     .attr("transform", `translate(${svgWidth * 0.15} 10)`)
 
-
-
+  // Add g elemet to append lines to
   svgLine1.append("g").attr("class", "lines")
 
-
+  // X-axis, since this is constant troughout all data
   var xAxis = d3.axisBottom(xScale).ticks(5).tickFormat(d3.format("0"));
 
-
+  // Add axis to svg
   svgLine1.append("g")
     .attr("class", "xAxis1")
     .attr("transform", `translate(0 ${heightLine1})`)
@@ -402,22 +423,24 @@ function initializeLine1Container() {
     .attr("fill", "#000")
     .text("Million kWh");
 
-
+  // Load first data shown in linechart-1
   loadDataLine1();
   }
 
 
 function loadDataLine1() {
+
+  // Remove old line-groups, if any
   d3.selectAll(".line-group1").remove()
 
+  // Get relevant values needed for linechart
   var dataLine1 = filterData("line1")
 
   var yValues = getValues(dataLine1, categoryOption);
 
-
   var maxY = Math.max.apply(Math, yValues);
 
-
+  // Get right size of svg for scales
   var margin = {top: 50, right: 50, left: 50, bottom: 50},
     widthLine1 = svgWidth - margin.left - margin.right,
     heightLine1 = 350 - margin.top - margin.bottom;
@@ -428,12 +451,14 @@ function loadDataLine1() {
 
   var yScale = d3.scaleLinear().domain([0, maxY]).range([heightLine1, 0]);
 
+  // Gives coordinates the line should follow
   var line = d3.line()
   .x(d => xScale(d.Year))
   .y(d => yScale(d.Quantity));
 
 
   // From https://bl.ocks.org/mbostock/5649592
+  // Functions for line-transitions
   function transition(path) {
     path.transition()
         .duration(750)
@@ -446,46 +471,51 @@ function loadDataLine1() {
           return i(t); };
     }
 
-
-
+  // Add lines-groups and line-paths
   var lines = d3.select(".lines")
   lines.selectAll(".line-group1")
     .data(dataLine1).enter()
     .append('g')
       .attr('class', 'line-group1')
-
     .append('path')
     .attr('class', 'lineLine1')
 
-  var toolTip = d3.tip()
-    .attr("class", "toolTip")
+  // Tooltip showing region, year and category values, bound to dots
+  var toolTipDot = d3.tip()
     .offset([-10, 0])
     .html(function(d) {
-      return "<span class='toolInfo'>" + d.categoryTag + "<br/>" + d.Year + ": " + d.Quantity  + " GWh " + "</span>";
+      return ("<span class='toolInfo'>" + d.categoryTag + "<br/>" + d.Year
+              + ": " + d.Quantity  + " GWh " + "</span>");
     })
 
+  // Tooltip showing only region information, bound to line
   var toolTipLine = d3.tip()
-    .attr("class", "toolTip")
     .offset([-10, 0])
     .html(function(d) {
       return "<span class='toolInfo'>" + d + "</span>";
     })
 
-    d3.select(".line1").call(toolTip)
-    d3.select(".line1").call(toolTipLine)
+  // Call tooltips
+  d3.select(".line1").call(toolTipDot)
+  d3.select(".line1").call(toolTipLine)
 
   var maxValue = getHighestValue(dataLine1)
 
-console.log(dataLine1)
+  // Give values to lines
   d3.selectAll(".lineLine1").data(dataLine1)
     .attr('d', d => line(d.values))
+
+    // Add transition
     .call(transition)
 
+    // Add correct color
     .style('stroke', function(d) {
       return colorFunction(maxValue, getMeanValue(d.values))
     })
     .style("stroke-width", "3px")
     .style("fill", "transparent")
+
+    // Show line tooltip and accentuate line
     .on("mouseover", function(d){
       toolTipLine.show(d.categoryTag)
         .style("left", `${d3.event.pageX}px`)
@@ -494,6 +524,8 @@ console.log(dataLine1)
         .style("stroke", "#74ffb0")
         .style("stroke-width", "4px")
     })
+
+    // Hide tooltip and accentuation
     .on("mouseout", function() {
       toolTipLine.hide()
       d3.select(this)
@@ -503,9 +535,13 @@ console.log(dataLine1)
         .style("stroke-width", "4px")
     })
 
+  // Remove old dots, if any
   d3.selectAll(".circle").remove()
 
+  // Variable to remember linecolor of accantuated line
   var parentColor = null;
+
+  // Add circles to lines
   d3.selectAll(".line-group1").selectAll(".circle")
     .data(function(d) { return d.values })
     .enter()
@@ -519,131 +555,146 @@ console.log(dataLine1)
       .attr("fill", function(d) {
         return d3.select(this.parentNode).select(".lineLine1").style("stroke")
       })
+
+      // On hover-over, show tooltip and accentuate dots
       .on("mouseover", function(d) {
-        toolTip.show(d)
+        toolTipDot.show(d)
         .style("left", `${d3.event.pageX}px`)
         .style("top", `${d3.event.pageY - 50}px`)
         parentColor = d3.select(this.parentNode).select(".lineLine1").style("stroke")
-
         d3.select(this.parentNode).select(".lineLine1").style("stroke", "#74ffb0")
 
       })
+
+      // Hides tooltip and accentuation
       .on("mouseout", function(d) {
-        toolTip.hide()
+        toolTipDot.hide()
         d3.select(this.parentNode).select(".lineLine1").style("stroke", parentColor)
       })
 
+  // Add Y-axis to chart
   var yAxis = d3.axisLeft(yScale).ticks(5);
-
   d3.select(".yAxis1").call(yAxis)
-  }
+}
 
 
 function initializeLine2Container() {
-    d3.selectAll(".line2").remove();
 
-    var dataLine2 = filterData("line2")
+  // Remove old linechart-2-svg
+  d3.selectAll(".line2").remove();
 
-    var yValues = getValues(dataLine2, categoryOption);
+  // Get relevant values for linechart-2
+  var dataLine2 = filterData("line2")
 
+  var yValues = getValues(dataLine2, categoryOption);
 
-    var maxY = Math.max.apply(Math, yValues);
+  var maxY = Math.max.apply(Math, yValues);
 
-    var margin = {top: 50, right: 50, left: 50, bottom: 50},
-      widthLine2 = svgWidth - margin.left - margin.right,
-      heightLine2 = 350 - margin.top - margin.bottom;
+  // Sizing up of the linechart-2-svg
+  var margin = {top: 50, right: 50, left: 50, bottom: 50},
+    widthLine2 = svgWidth - margin.left - margin.right,
+    heightLine2 = 350 - margin.top - margin.bottom;
 
-    var parseTime = d3.timeParse("%Y");
+  // Get correct scales for axis and values
+  var parseTime = d3.timeParse("%Y");
 
-    var xScale = d3.scaleTime().domain([1990, 2014]).range([0, widthLine2 * .85]);
+  var xScale = d3.scaleTime().domain([1990, 2014]).range([0, widthLine2 * .85]);
 
-    var yScale = d3.scaleLinear().domain([0, maxY]).range([heightLine2, 0]);
+  var yScale = d3.scaleLinear().domain([0, maxY]).range([heightLine2, 0]);
 
-    var line2 = d3.line()
-    .x(d => xScale(d.Year))
-    .y(d => yScale(d.Quantity));
+  // Returns coordinates for lines
+  var line2 = d3.line()
+  .x(d => xScale(d.Year))
+  .y(d => yScale(d.Quantity));
 
+  // Add svg
+  var svgLine2 = d3.select("#line2Div")
+    .append("svg")
+    .attr("width", `${svgWidth}`)
+    .attr("height", `${svgHeight}`)
+    .attr("class", "line2")
+    .append("g")
+    .attr("transform", `translate(${svgWidth * 0.15} 10)`)
 
+  // Make axes
+  var xAxis = d3.axisBottom(xScale).ticks(5).tickFormat(d3.format("0"));
 
-    var svgLine2 = d3.select("#line2Div")
-      .append("svg")
-      .attr("width", `${svgWidth}`)
-      .attr("height", `${svgHeight}`)
-      .attr("class", "line2")
-      .append("g")
-      .attr("transform", `translate(${svgWidth * 0.15} 10)`)
+  var yAxis = d3.axisLeft(yScale).ticks(5);
 
+  // Add axes to linechart-2
+  svgLine2.append("g")
+    .attr("class", "xAxis2")
+    .attr("transform", `translate(0 ${heightLine2})`)
+    .call(xAxis)
+    .append("text")
+    .attr("x", widthLine2 * .85 / 2)
+    .attr("y", 30)
+    .attr("fill", "#000")
+    .text("Years");
 
+  svgLine2.append("g")
+    .attr("class", "yAxis2")
+    .call(yAxis)
+    .append('text')
+    .attr("y", 15)
+    .attr("transform", "rotate(-90)")
+    .attr("fill", "#000")
+    .text("Million kWh");
 
-
-
-    var xAxis = d3.axisBottom(xScale).ticks(5).tickFormat(d3.format("0"));
-
-    var yAxis = d3.axisLeft(yScale).ticks(5);
-
-
-    svgLine2.append("g")
-      .attr("class", "xAxis2")
-      .attr("transform", `translate(0 ${heightLine2})`)
-      .call(xAxis)
-      .append("text")
-      .attr("x", widthLine2 * .85 / 2)
-      .attr("y", 30)
-      .attr("fill", "#000")
-      .text("Years");
-
-    svgLine2.append("g")
-      .attr("class", "yAxis2")
-      .call(yAxis)
-      .append('text')
-      .attr("y", 15)
-      .attr("transform", "rotate(-90)")
-      .attr("fill", "#000")
-      .text("Million kWh");
-
-      loadDataLine2();
+  // Load first data shown in linechart-2
+  loadDataLine2();
 }
 
 
 function loadDataLine2() {
 
+  // Remove old line-groups, if any
   d3.selectAll(".line-group2").remove();
 
+  // Get svg-sizes
   var margin = {top: 50, right: 50, left: 50, bottom: 50},
     widthLine2 = svgWidth - margin.left - margin.right,
     heightLine2 = 350 - margin.top - margin.bottom;
 
+  // Get data relevant for this linechart
   var dataLine2 = filterData("line2");
 
   var yValues = getValues(dataLine2, categoryOption);
 
-
   var maxY = Math.max.apply(Math, yValues);
 
+  // Get correct scales for axis and lines
   var parseTime = d3.timeParse("%Y");
 
   var xScale = d3.scaleTime().domain([1990, 2014]).range([0, widthLine2 * 0.85]);
 
   var yScale = d3.scaleLinear().domain([0, maxY]).range([heightLine2, 0]);
 
+  // Returns line coordinates
   var line2 = d3.line()
   .x(d => xScale(d.Year))
   .y(d => yScale(d.Quantity));
 
+  // Make Y-axis
   var yAxis = d3.axisLeft(yScale).ticks(5);
 
+  // Functions for line-transitions
+  // From https://bl.ocks.org/mbostock/5649592
   function transition(path) {
     path.transition()
         .duration(750)
         .attrTween("stroke-dasharray", tweenDash);
     }
-    function tweenDash() {
-        var l = this.getTotalLength(),
-          i = d3.interpolateString("0," + l, l + "," + l);
-        return function (t) {
-          return i(t); };
+
+  function tweenDash() {
+      var l = this.getTotalLength(),
+        i = d3.interpolateString("0," + l, l + "," + l);
+      return function(t) {
+        return i(t);
+      };
     }
 
+    // Add line-groups to svg
   var lines = d3.select(".line2").append("g").attr("class", "lines")
   lines.selectAll(".line-group2")
     .data(dataLine2).enter()
@@ -651,36 +702,17 @@ function loadDataLine2() {
       .attr('class', 'line-group2')
       .attr("transform", `translate(${svgWidth * 0.15} 10)`)
 
-
+    // Add paths to line-groups with coordinates
     .append('path')
     .attr('class', 'PathLine2')
     .attr('d', d => line2(d.values))
     .call(transition)
-
     .style('stroke', "black")
     .style("stroke-width", "3px")
     .style("fill", "transparent")
 
 
-   function getHighestValue(data) {
-     bigValuesList = []
-     for (cat of data) {
-       smallValuesList = [];
-       for (value of cat.values) {
-         smallValuesList.push(value.Quantity)
-       }
-       bigValuesList.push(d3.mean(smallValuesList))
-     }
-     return Math.max.apply(Math, bigValuesList)
-   }
 
-   function getMeanValue(values) {
-     smallValuesList = [];
-     for (value of Object.values(values.values)) {
-       smallValuesList.push(value.Quantity)
-     }
-     return d3.mean(smallValuesList)
-   }
 
   var maxValue = getHighestValue(dataLine2)
 
